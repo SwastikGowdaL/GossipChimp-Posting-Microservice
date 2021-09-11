6eebe2cd6b3eb5df3f957141e5a9f7da540c782d
swastik <swastik7999@gmail.com>
2021-09-11 20:03:52 +0530

    :art: add comments for config file

--- config/config.js
+++ config/config.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
+  //* env variable will be either dev or test
+  ENV: process.env.ENV,
+
+  //* port will be 3000
   PORT: process.env.PORT,
+
+  //* db conn for gossipChimp and another db conn while testing
   GOSSIP_CHIMP: process.env.GOSSIP_CHIMP,
   GOSSIP_CHIMP_TEST: process.env.GOSSIP_CHIMP_TEST,
-  ENV: process.env.ENV,
+
+  //* auth key for requests
   AUTH_KEY: process.env.AUTH_KEY,
+
+  //* credentials for imageKit.io
   IMAGE_KIT: {
     publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
     privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
