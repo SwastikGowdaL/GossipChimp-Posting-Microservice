8d4cc008574b8564b86b6ac279df1b9eb8bd503c
swastik <swastik7999@gmail.com>
2021-09-11 19:11:32 +0530

    :clown_face: add mocks for ImageKit

---
+++ components/posting/__mocks__/imagekit.js
@@ -0,0 +1,31 @@
+const config = require('../../../config/config');
+
+class ImageKit {
+  constructor(keys) {
+    this.publicKey = keys.publicKey;
+    this.privateKey = keys.privateKey;
+    this.urlEndpoint = keys.urlEndpoint;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  upload(file, cb) {
+    if (
+      this.publicKey === config.IMAGE_KIT.publicKey &&
+      this.privateKey === config.IMAGE_KIT.privateKey &&
+      this.urlEndpoint === config.IMAGE_KIT.urlEndpoint
+    ) {
+      const result = {
+        url: 'url',
+      };
+      cb(undefined, result);
+    }
+
+    const err = {
+      status: 'Error',
+      message: 'Invalid credentials',
+    };
+    cb(err, undefined);
+  }
+}
+
+module.exports = ImageKit;
