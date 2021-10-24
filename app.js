443e46484819570e078d138e89c295a308918c4d
swastik <swastik7999@gmail.com>
2021-10-24 12:05:01 +0530

    :lock: fix security issues

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 require('./db/mongoose');
 const posting = require('./components/posting');
 
 const app = express();
+app.use(helmet());
 app.use(express.json());
 app.use(cors());
 
